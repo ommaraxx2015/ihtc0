2d35d1e775a3b665c4c48323b13b533158cfcd74
ommaraxx2015 <76623269+ommaraxx2015@users.noreply.github.com>
2021-01-15 01:32:35 +0200

    Create FACK_YOUR_MATHERS_YOUR_THEF_HAHAHAHAHA.js

---
+++ FACK_YOUR_MATHERS_YOUR_THEF_HAHAHAHAHA.js
@@ -0,0 +1,50 @@
+
+<-- كسم كل واحد يسرق الستايل -->
+<-- كسم كل العراقيين ولاد زانيات -->
+<-- الستايل تصميم عمر الحديد ضاغط شرفكم يولاد زانيا -->
+"https:" != location.protocol && (location.protocol = "https:")
+$(`<img style="width:400px;margin-bottom: 298px;" >
+<center>
+<div id="العراقيين_ولاد_قحبة_سراقين_ههههه">
+<a id="OMARALHADED21" href=""style="width: 62px;padding:0;height: 20px;" class="btn minix btn-primary">اتصل بنا</a>
+<a id="OMARALHADED21" href="" style="width: 62px;padding:0;height: 20px;" class="btn minix btn-primary">التطبيق</a>
+<a id="OMARALHADED21" href=""style="width: 62px;padding:0;height: 20px;" class="btn minix btn-primary">القوانين</a>
+<a id="OMARALHADED21" href=""style="width: 62px;padding:0;height: 20px;" class="btn minix btn-primary">اشتراكات</a>
+<a id="OMARALHADED21" href=""style="width: 85px;padding:0;height: 20px;" class="btn minix btn-primary">  شات احبك</a>
+<a id="OMARALHADED21" href=""style="width: 50px;padding:0;height: 20px;" class="btn minix btn-primary">راديو</a><a id="OMARALHADED25"></a><a id="OMARALHADED27"></a></div></div><center>`).insertBefore(".nav-tabs")
+
+<-- كسم كل واحد يسرق الستايل -->
+<-- كسم كل العراقيين ولاد زانيات -->
+<-- الستايل تصميم عمر الحديد ضاغط شرفكم يولاد زانيا -->
+
+$(".nav-tabs").addClass("fl").css("width", "100%")
+var nopm = !0
+    , nonot = !0;
+$(".hand.nosel.fl.uzr.border").css({ margin: "1px 0", borderRadius: "0px" }), 
+$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><i class="fa fa-eye-slash" aria-hidden="true"></i>`)
+$(`<a href="" target="_blank" style="width:107px;color: #E8415D;text-decoration:none; margin-right: 5px;margin-left: 2px;text-align: center;float: right;border-radius: 5px;border-color: #F2F2F2;padding: 3px;" class="border" ><i class="fa fa-android"></i>&nbsp;التطبيق</a>`).insertBefore(".umute")
+$(".nav-tabs").addClass("fl").css("width", "100%");
+var _0xb650 = ["maxlength", "50", "attr", "textarea#tbox.fl.corner.tbox.filw"];
+$(_0xb650[3])[_0xb650[2]](_0xb650[0], _0xb650[1]);
+var _0xb650 = ["maxlength", "10", "attr", "div#tlogins input"];
+$(_0xb650[3])[_0xb650[2]](_0xb650[0], _0xb650[1]), 
+$("div#tlogins button.btn.btn-primary")
+    .click(function () { var b = setInterval(function () { var c = $(".pmsgc")
+                .length;
+            0 < c ? ($(`</iframe></div><center><center><audio src="https://iraqiachats.com/iraqna.mp3" controls loop></audio></div><center>`)
+                .appendTo("#mic"), clearInterval(b)) : console.log(c) }, 1) }), $("div#tlogins button.btn.btn-primary")
+    .click(function () { setTimeout(function () { refreshonline() }, 0) }), setInterval(function () { var a = $("#dpnl");
+        0 < a.length && a.remove() }, 0), $(".dpnl")
+    .append(` <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"><center></div><center></div>`), eval(function (b, f, a, g, h, i) { if (h = function (a) { return (a < f ? "" : h(parseInt(a / f))) + (35 < (a %= f) ? String.fromCharCode(a + 29) : a.toString(36)) }, !"".replace(/^/, String)) { for (; a--;) i[h(a)] = g[a] || h(a);
+            g = [function (a) { return i[a] }], h = function () { return "\\w+" }, a = 1 } for (; a--;) g[a] && (b = b.replace(new RegExp("\\b" + h(a) + "\\b", "g"), g[a])); return b }("$('#C').d(`<1 6=\"\\4\\e\\f \\2\\g\\5\\h\\i\\2\\j \\2\\k\\4\\2\\5\"n=\"#\"c=\"$('.p').o($(8).r('6'));s($(8),'a');u(v(){$('#3').w(0);},0);$('.x').y();\"7-z=\"A\"7-q=\"#3\"b=\"B 9 1 1-a 9-m-l\">&t;</1>`);", 39, 39, ["", "label", "u0627", "mic", "u0634", "u062A", "title", "data", "this", "fa", "primary", "class", "onclick", "append", "u0631", "u062D", "u0633", "u062E", "u062F", "u0645", "u0644", "up", "volume", "href", "text", "pnhead", "target", "attr", "hl", "nbsp", "setTimeout", "function", "scrollTop", "dpnl", "show", "toggle", "tab", "ae", "d0"], 0, {}))
+$(".ae.fa.label.label-primary.fa-users")
+    .text('آلــغرف');
+$('.nav-tabs').addClass('fl').css('width', '100%');
+$(`<style>div.break.fl::before{content:" اخي الزائر اختي الزائره قم بإيقاف برنامج تغيير الآي بي حتى تتمكن من دخول الدردشة او مراسلتنا من صفحة اتصل بنا  ";color:#D41515}</style>`).insertBefore('.loginstat');
+$(`<link rel="stylesheet" href="https://rawcdn.githack.com/ommaraxx2015/ihtc0/3b4db25cf5c7843a9443c00eba3bca1fcb4078ea/FACK_YOUR_MARHERS_Iraq.css"><style>.u-msg.break.fl { line-height: 26px!important; }</style>`).insertBefore("body");
+$(`<div><marquee direction="right" id="OA43"scrollamount="3">آهلا وسهلا بكم في شات احبك للجوال  <font class="a"> ● ·</font>   نورت وللجميع بأطيب الاوقات&nbsp;</marquee><div>`).insertBefore('div#tlogins .lonline.light.break');
+
+
+<-- كسم كل واحد يسرق الستايل -->
+<-- كسم كل العراقيين ولاد زانيات -->
+<-- الستايل تصميم عمر الحديد ضاغط شرفكم يولاد زانيا -->
